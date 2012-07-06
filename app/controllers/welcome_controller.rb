53425c1dd7446fb46eceecb279c532828e802cdb
Dylan Griffin <griffindy@gmail.com>
2012-07-06 14:25:27 -0400

    signed in redirect wasn't working, changed to simpler method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,8 +8,7 @@ class WelcomeController < ApplicationController
 
   def check_for_user
     if signed_in?
-      #this is ugly, especially with the s adding on after the type
-      redirect_to current_user.becomes(current_user.type.constantize)
+      redirect_to current_user
     else
       redirect_to sign_in_path
     end
