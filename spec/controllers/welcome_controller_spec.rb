86e16a27fc68c50d8c9c038cc2f7a36152585ac9
Dylan Griffin <griffindy@gmail.com>
2012-07-09 09:36:08 -0400

    updated welcome controller spec with new redirects

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,17 +2,22 @@ require "spec_helper"
 
 describe WelcomeController do
 
+  it "should go to the index if not signed in" do
+    get :index
+    response.should be_success
+  end
+
   it "redirects to the landlord's list if a landlord" do
     landlord = create(:landlord)
     sign_in_as(landlord)
     get :index
-    response.should redirect_to(landlord_path(landlord.id))
+    response.should redirect_to(dashboard_path)
   end
 
   it "redirects to the tenant's favorites if a tenant" do
     tenant = create(:tenant)
     sign_in_as(tenant)
     get :index
-    response.should redirect_to(tenant_path(tenant.id))
+    response.should redirect_to(dashboard_path)
   end
 end
