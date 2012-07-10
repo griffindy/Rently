c90515698abaf48830ed03a6a5df2e00b8ca4a88
Dylan Griffin <griffindy@gmail.com>
2012-07-10 11:23:54 -0400

    updated tests to work with new code

--- spec/models/landlord_spec.rb
+++ spec/models/landlord_spec.rb
@@ -1,10 +1,4 @@
 require 'spec_helper'
 
 describe Landlord do
-  it "#can_edit should return false if you are not the landlord of an apartment" do
-    landlord = create(:landlord)
-    apt = create(:apartment)
-    current_user = create(:user, name: "Dylan Griffin", type: "Tenant")
-    apt.can_edit?(landlord, current_user).should be_false
-  end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -4,5 +4,5 @@ describe Photo do
   it { should have_attached_file(:photo) }
   it { should validate_attachment_presence(:photo) }
   it { should validate_attachment_content_type(:photo).
-    allowing('image/png', 'image/gif', 'image/jpg') }
+    allowing('image/png', 'image/gif', 'image/jpeg', 'image/pjpeg') }
 end
